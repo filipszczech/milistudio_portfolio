12893ed748316253485ec2ddace7dac167e9f722
KOMPUTER <filipszczech4@gmail.com>
2025-01-20 17:40:29 +0100

    chore: text update

--- nuxt-supabase-template/pages/about.vue
+++ nuxt-supabase-template/pages/about.vue
@@ -18,7 +18,7 @@
                         :duration="600" 
                         :delay="400"
                     >murzyn kurier rowerowy we własnej osobie</p> -->
-                    <p class="text-center text-lg lg:text-xl max-w-2xl my-6">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+                    <p class="text-center text-lg lg:text-xl max-w-2xl my-6">Michał Lichtański murzyn kurier rowerowy we własnej osobie. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
                     <h2 class="text-4xl md:text-6xl font-semibold text-center mb-6">linki</h2>
                     <a href="https://www.instagram.com/lichtanskimichal/" target="_blank" rel="noopener" aria-label="Michał Lichtański Instagram Profile" class="flex gap-2 items-center text-lg lg:text-xl pb-1 border-b border-black w-fit"
                         v-motion
